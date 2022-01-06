b5bf816cf9995689641abca2ca6daf77a9d83c26
Riko Pernando <rikopernando96@gmail.com>
2022-01-06 17:45:51 +0700

    create component

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import logo from '../logo.svg';
+
+function Header() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn Reactsdfsd
+      </a>
+    </header>
+  )
+}
+
+Header.propTypes = {
+
+}
+
+export default Header
+
